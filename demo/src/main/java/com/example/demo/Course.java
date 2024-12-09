652e6d9ea5f73adbe0b06128ae6b4fd4e6661aa3
Ilaydabuzbuz <ilaydabuzbuz@hotmail.com>
2024-12-09 23:07:20 +0300

    small adjustments in Course Class

--- demo/src/main/java/com/example/demo/Course.java
+++ demo/src/main/java/com/example/demo/Course.java
@@ -3,40 +3,42 @@ package com.example.demo;
 import com.almasb.fxgl.core.collection.Array;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Course implements IGeneric {
-    private String courseName;
-    private int courseID;
+    private String courseID;
     private String timeToStart;
     private int duration;
+    private String lecturerName;
+    private ArrayList<String> studentNames;
+    private String courseDay;
     private LocalTime startTime;
     private LocalTime endTime;
-    private String lecturerName;
     private Lecturer lecturer;
     private Classroom assignedClassroom;
-    private ArrayList<String> studentNames;
     private ArrayList<Student> enrolledStudentsList;
     private ArrayList<Attendance> attendanceRecordList;
-    private String courseDay;
 
     //TODO: Maalesefki parametrelerde olan lecturer ve student objelerini String ile değiştirmem gerekti çünkü
     //TODO: csv den sadece öğrencinin ve öğretmenin adını çekebiliyoruz ID'lerini bulabileceğimiz bir data kaynagı yok tahminimce login ekranında
     //TODO: Öğrenci ya da Öğretmen register olurken bu ID datası sistemimize gelicek ve o zaman Lecturer ve Student Objeleri oluşturabilicez
     //TODO: Şu anlık bu sebeplerden dolayı referansları değiştridim eğer bu sorun aklınıza çözüm gelirse lütfen değiştirin.
 
-    public Course(String courseName, String timeToStart,int duration, String lecturerName, ArrayList<String> studentNames) {
-        this.courseName = courseName;
-        this.timeToStart = timeToStart;
+    public Course(String courseID, String timeToStart,int duration, String lecturerName, ArrayList<String> studentNames) {
+        this.courseID = courseID;
+        this.duration = duration;
+        this.lecturerName = lecturerName;
+        this.studentNames = studentNames;
+        //this.timeToStart = timeToStart;
 
         /*
         day ve startTime timeToStart Attributendan arada " " (boşluk) ile pars edilerek initialize edilicek
         çünkü csv'deki parametre gün ve başlama saatini ";" ile ayırmamış.
          */
-
-        this.duration = duration;
-        this.lecturerName = lecturerName;
-        this.studentNames = studentNames;
+        String[] timeParts = timeToStart.split(" ");
+        this.courseDay = timeParts[0];
+        this.startTime = LocalTime.parse(timeParts[1], DateTimeFormatter.ofPattern("HH:mm"));
         this.endTime = this.getEndTime(startTime);
 
         //TODO Person için gerekli olan bütün attributelar sağlandıtan sonra Student ve Lecturer Objeleri oluşturulup Course Objesinin gerekli attributeları ile initialize edilmeli.
@@ -44,7 +46,7 @@ public class Course implements IGeneric {
     }
 
     public LocalTime getEndTime(LocalTime startTime) {
-        int totalDuration = duration*45 + (duration - 1)*10;
+        int totalDuration = duration * 45 + (duration - 1) * 10;
         endTime = startTime.plusMinutes(totalDuration);
         return endTime;
     }
@@ -66,5 +68,9 @@ public class Course implements IGeneric {
 
     public LocalTime getStartTime() {
         return startTime;
+    }
+
+    public String getCourseDay() {
+        return courseDay;
     }
 }
