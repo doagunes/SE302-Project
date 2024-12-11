b904d53a815f07a4b6c29da36680146a4003c652
Doğa Güneş <120034878+doagunes@users.noreply.github.com>
2024-12-11 21:56:34 +0300

    Classroom data run succesfully

--- demo/src/main/java/com/example/demo/DatabaseConnection.java
+++ demo/src/main/java/com/example/demo/DatabaseConnection.java
@@ -5,19 +5,14 @@ import java.sql.*;
 public class DatabaseConnection {
 
     private static final String url = "jdbc:sqlite:projectdb.db";
-    private static Connection connection;
 
-    public static Connection getConnection(){
-
-        if(connection == null){ // singleton
-            try {
-                connection = DriverManager.getConnection(url); // connected to database
-                System.out.println("Connected to database!");
-            }catch (SQLException e){
-                System.out.println("Connection error: " + e.getMessage());
-            }
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println("Connection error: " + e.getMessage());
+            throw new RuntimeException(e);
         }
-
-        return connection;
     }
+
 }
