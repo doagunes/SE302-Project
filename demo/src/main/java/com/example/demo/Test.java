eccf38f9216f8bc9e1f2caf37e464920732f8332
Doğa Güneş <120034878+doagunes@users.noreply.github.com>
2024-12-12 00:10:12 +0300

    for tables drop table added and then made them comment

--- demo/src/main/java/com/example/demo/Test.java
+++ demo/src/main/java/com/example/demo/Test.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
         CSV_Reader csv = new CSV_Reader();
+
         /*
         ClassroomDataAccessObject cdo = new ClassroomDataAccessObject();
         cdo.createTable();
         cdo.addClassroom(csv.readClassrooms());
         cdo.getClassrooms();
+
          */
 
 
@@ -17,7 +19,6 @@ public class Test {
         courseDataAccessObject.createTable();
         courseDataAccessObject.addCourse(csv.readCourses());
         courseDataAccessObject.getCourses();
-
 
     }
 }
