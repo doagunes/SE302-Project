61b7bcdf28bd8e611980eff999a7e4462bb53017
Doğa Güneş <120034878+doagunes@users.noreply.github.com>
2024-12-21 05:14:47 +0300

    some changes

--- demo/src/main/java/com/example/demo/CourseDataAccessObject.java
+++ demo/src/main/java/com/example/demo/CourseDataAccessObject.java
@@ -13,7 +13,7 @@ public class CourseDataAccessObject {
 
     public static void createTable() {
 
-
+/*
         String sql2 = "DROP TABLE IF EXISTS Course";
         try (Connection conn = DatabaseConnection.getConnection();
              Statement stmt = conn.createStatement()) {
@@ -22,6 +22,8 @@ public class CourseDataAccessObject {
         } catch (SQLException e) {
             System.out.println("Creating table error: " + e.getMessage());
         }
+        
+ */
 
 
 
